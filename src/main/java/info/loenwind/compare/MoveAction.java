d3ca58bccd3f762717e076f6b5c17ab7f09c2275
Frank Martin <fnitram@gmail.com>
2022-10-24 16:49:51 -0400

    Refactor

--- src/main/java/info/loenwind/compare/MoveAction.java
+++ src/main/java/info/loenwind/compare/MoveAction.java
@@ -22,21 +22,25 @@ public class MoveAction implements ActionListener{
         this.settings = settings;
     }
 
-    private String ScoreToFolder(Integer score){
+    private boolean isInRange(int value, int lower, int upper){
+        return (value >= lower && value < upper);
+    }
+
+    private String ScoreToFolderName(Integer score){
 
         String result;
         Integer scoreBy10 = (Integer)(score / 10);
-
+        
         if(scoreBy10 == 100){result = "A_100_Winners";}
-        else if(scoreBy10 >= 90 && scoreBy10  < 100){result = "B_90-99";}
-        else if(scoreBy10 >= 80 && scoreBy10 < 90){result = "C_80-89";}
-        else if(scoreBy10 >= 70 && scoreBy10 < 80){result = "D_70-79";}
-        else if(scoreBy10 >= 60 && scoreBy10 < 70){result = "E_60-69";}
-        else if(scoreBy10 >= 50 && scoreBy10 < 60){result = "F_50-59";}
-        else if(scoreBy10 >= 40 && scoreBy10 < 50){result = "G_40-49";}
-        else if(scoreBy10 >= 30 && scoreBy10 < 40){result = "H_30-39";}
-        else if(scoreBy10 >= 20 && scoreBy10 < 30){result = "I_20-29";}
-        else if(scoreBy10 >= 10 && scoreBy10 < 20){result = "J_10-19";}
+        else if(isInRange(scoreBy10, 90, 100)){result = "B_90-99";}
+        else if(isInRange(scoreBy10, 80, 90)){result = "C_80-89";}
+        else if(isInRange(scoreBy10, 70, 80)){result = "D_70-79";}
+        else if(isInRange(scoreBy10, 60, 70)){result = "E_60-69";}
+        else if(isInRange(scoreBy10, 50, 60)){result = "F_50-59";}
+        else if(isInRange(scoreBy10, 40, 50)){result = "G_40-49";}
+        else if(isInRange(scoreBy10, 30, 40)){result = "H_30-39";}
+        else if(isInRange(scoreBy10, 20, 30)){result = "I_20-29";}
+        else if(isInRange(scoreBy10, 10, 20)){result = "J_10-19";}
         else {result = "K_0-9";}
         
         return result;
@@ -45,17 +49,16 @@ public class MoveAction implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        String root = settings.getTargetFolder().toString();
+        String targetFolder = settings.getTargetFolder().toString();
         if(settings.isTargetFolderValid())
         {
             try{
                 for (int i = 0; i < owner.getTable().getRowCount(); i++) {
                     Integer score = (Integer)owner.getTable().getValueAt(i, 0);
                     File file = new File((String)owner.getTable().getValueAt(i,1));          
-                    Path fullPath = Paths.get(root,  ScoreToFolder(score));
-                    String fullPathStr = fullPath.toString();
-                    Files.createDirectories(Paths.get(fullPathStr));
-                    Files.move(Paths.get(file.getAbsolutePath()), Paths.get(fullPathStr, file.getName()));
+                    Path pathToScoreFolder = Paths.get(targetFolder,  ScoreToFolderName(score));
+                    Files.createDirectories(pathToScoreFolder);
+                    Files.move(Paths.get(file.getAbsolutePath()), Paths.get(pathToScoreFolder.toString(), file.getName()));
                 }
                 
             } catch (IOException err) {
@@ -63,6 +66,6 @@ public class MoveAction implements ActionListener{
             }
         }
 
-        JOptionPane.showMessageDialog(owner,"All files has been move to the target folder",root, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(owner,"All files has been moved to the target folder",targetFolder, JOptionPane.INFORMATION_MESSAGE);
     }
 }
